2dde2eae23ea72ed0f0393c65ee100244a1a28e3
Duong Nguyen <duongnguyen.dng01@gmail.com>
2026-05-12 02:02:07 +0700

    hhhh

--- app.py
+++ app.py
@@ -2365,4 +2365,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         _auto_import_when_empty()
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False,
+    )
